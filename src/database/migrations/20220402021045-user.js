496e56f8919acdbafaae94116893e66386504cb4
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-04-02 11:14:15 -0300

    user migrate

--- src/database/migrations/20220402021045-user.js
+++ src/database/migrations/20220402021045-user.js
@@ -2,20 +2,19 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
+    await queryInterface.createTable('user', {
+       id: Sequelize.STRING,
+       name: Sequelize.STRING,
+       email: Sequelize.STRING,
+       tel: Sequelize.STRING,
+      });
+     
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+    await queryInterface.dropTable('user');
+    
   }
 };
