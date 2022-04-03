7e323e8be46960d6f3e29fc79fed0f6dc68c799d
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-04-03 17:03:49 -0300

    correction

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -4,22 +4,19 @@ const UserModel = require("../database/models/UserModel");
 
 const UserController = {
   async store(req,res){
-    const id = uuidv4()
+    const id = uuidv4();
+    const {name,email,tel,msg} = req.body; 
 
-    console.log(id)
-    // const {name,email,tel,msg} = req.body; 
     const user = await UserModel.create({
-      id
-    }) 
-    // console.log(id,name,email,tel,msg)
+      id,name,email,tel,msg
+    }); 
 
-    return res.status(200).json(user);
+    return res.status(200).send('');
 
   },
   async read(req,res){
-    const user = await UserModel.findAll()
-
-    return res.status(200).send(user)
+    const user = await UserModel.findAll();
+    return res.status(200).json(user);
   }
 };
 
--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,7 @@ const Sequelize = require('sequelize');
 const dbConfig = require('./config/database');
 const UserModel = require('./models/UserModel');
 
-
 const conection = new Sequelize(dbConfig);
-
-
 
 UserModel.init(conection);
 
