8b3dc46ea7f39c24f740fdd5c62fb2c13ecfb076
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-04-05 16:13:41 -0300

    basic auth

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan')
-const useRoute = require('./routes/user.routes');
-
 require('dotenv-safe').config();
 require('./database');
 
+const useRoute = require('./routes/user.routes');
+
 const app = express();
 
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
