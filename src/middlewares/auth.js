4615b8fafb35b9c8eee611e0245c9351a8ccbedb
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-04-06 07:57:01 -0300

    correction middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,11 @@
 module.exports = (req,res,next) => {
 
   const key  = req.body.loginKey
-  if(key === process.env.LOGINKEY){
-    res.status(200).json({messge:"Connection sucess "})
-
+  
+  if(key !== process.env.LOGINKEY){
+    res.status(401).json({"message":"unauthorized"})
+  }else{
+    res.status(200)
     next()
   }
-  res.status(401).json({message:"Unauthorized"})
 }
